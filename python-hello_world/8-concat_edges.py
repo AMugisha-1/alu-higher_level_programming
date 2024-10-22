509dce76d45306d05c73fa3e03cdb3e65569b72e
AMugisha-1 <a.mugisha1@alustudent.com>
2024-10-22 07:07:46 -0700

    eigth commit

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str1 = "object"
+str2 = "-oriented "
+str3 = "programming"
+str4 = " with "
+str5 = "Python"
+print(str1 + str2 + str3 + str4 + str5)
+print(str)
