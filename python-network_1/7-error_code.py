6bd05c0f9b105b82bbecbef53877708795d910b4
AMugisha-1 <a.mugisha1@alustudent.com>
2024-11-25 23:50:03 -0800

    python-network_1

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""
-Module 7-error_code.py
-"""
-
-
+"""send to a given url and displays the response body"""
+import sys
 import requests
-from sys import argv
+
 
 if __name__ == "__main__":
-    resp = requests.get(argv[1])
-    if resp.status_code >= 400:
-        print("Error code: {}".format(resp.status_code))
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
     else:
-        print(resp.text)
+        print(req.text)
