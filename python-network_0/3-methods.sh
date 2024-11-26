88934ad10bddf65c60325ffb2073c591af015674
AMugisha-1 <a.mugisha1@alustudent.com>
2024-11-25 22:15:06 -0800

    network

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL and displays all HTTP methods the server will accept
-curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
+# Script to test if the server supports OPTIONS, HEAD, GET, POST, PUT, DELETE methods
+curl -sI -X OPTIONS "$1" | grep "Allow" | cut -d ' ' -f2-
