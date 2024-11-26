302df7281367f454ccfed078c52200ff8d0d07b6
AMugisha-1 <a.mugisha1@alustudent.com>
2024-11-25 23:24:50 -0800

    python-network_1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
+"""
+Script to fetch and display status information using urllib.
+Fetches data from a given URL and prints the X-Request-Id header.
+"""
+
 import urllib.request
 
 url = 'https://intranet.hbtn.io/status'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
-    '\n    Chrome/99.0.4844.84 Safari/537.36',
-}
 
-req = urllib.request.Request(url, headers=headers)
-with urllib.request.urlopen(req) as response:
-    content = response.read()
+# Fetch the URL using urllib
+with urllib.request.urlopen(url) as response:
+    body = response.read()
     print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode("utf-8"))
+    print(f"\t- type: {type(body)}")
+    print(f"\t- content: {body}")
+    print(f"\t- utf8 content: {body.decode('utf-8')}")
