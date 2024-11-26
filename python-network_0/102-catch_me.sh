02636b41689839778867fe2b9e6c21ec8e330fa9
AMugisha-1 <a.mugisha1@alustudent.com>
2024-11-25 23:13:13 -0800

    network

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+# This script uses curl to send a GET request to the server and handle the response.
 curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
