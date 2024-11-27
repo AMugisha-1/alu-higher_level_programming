6ef9533d6ba1754e3da2542ba81af873a0e172ba
AMugisha-1 <a.mugisha1@alustudent.com>
2024-11-27 06:14:49 -0800

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,4 +1,29 @@
-# base.py: Base classes and utilities for the project
+#!/usr/bin/python3
+"""
+This module defines the `Base` class, which serves as the base class
+for other models in this project.
+"""
 
 class Base:
-    pass
+    """
+    A base class to manage unique IDs for instances.
+
+    Attributes:
+        __nb_objects (int): Tracks the number of objects created.
+        id (int): Unique identifier for the instance.
+    """
+    __nb_objects = 0  # Private class attribute to track objects
+
+    def __init__(self, id=None):
+        """
+        Initializes a new instance of the Base class.
+
+        Args:
+            id (int, optional): If provided, sets the ID for the instance.
+                               Otherwise, assigns a new unique ID.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
